43dd05cee434a6df19db5c72d5e208128961a7fe
nk-on <johndoe@example.com>
2024-07-10 17:11:48 +0400

    Choosing html elements with javascript

---
+++ src/script.js
@@ -0,0 +1,6 @@
+const numberInput = document.querySelector('number-input');
+const getImageButton = document.querySelector('[data-get-images]');
+const getRandomAmountOfImage = document.querySelector(
+  '[data-get-random-images]'
+);
+const picturesContainer = document.querySelector('[data-pictures-container]');
