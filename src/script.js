b8be8950f553d74c0f42d7940ff59424aef03232
nk-on <johndoe@example.com>
2024-07-10 17:40:46 +0400

    Adding search inputs and buttons for searhcing particular image

--- src/script.js
+++ src/script.js
@@ -1,6 +1,8 @@
 const numberInput = document.querySelector('number-input');
+const searchInput = document.querySelector('#search-input')
 const getImageButton = document.querySelector('[data-get-images]');
 const getRandomAmountOfImage = document.querySelector(
   '[data-get-random-images]'
 );
+const searchImagesButton = document.querySelector('[data-search-images]')
 const picturesContainer = document.querySelector('[data-pictures-container]');
