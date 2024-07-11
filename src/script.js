04ad301d65188c2d6d68f625ddc2970eabea2e1d
nk-on <2nkoz@proton.me>
2024-07-10 18:48:14 -0700

    Feat:fetching API

--- src/script.js
+++ src/script.js
@@ -6,3 +6,13 @@ const getRandomAmountOfImage = document.querySelector(
 );
 const searchImagesButton = document.querySelector('[data-search-images]')
 const picturesContainer = document.querySelector('[data-pictures-container]');
+async function getImages(e){
+  e.preventDefault();
+  const res = await fetch("https://api.pexels.com/v1/curated", {
+    headers: {
+      Authorization: "uJ7wfW86udCYLqHY1cmNI5OfZD8V8UroDvM7ERHWXvXOkI5YL2V5z2WF"
+    }
+  });
+  const data = await res.json();
+};
+getImageButton.addEventListener('click',getImages);
